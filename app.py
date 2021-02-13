4ac605009cc0bec0124545676e6bb0d995e50148
Gamattowicz <p.romanczuk31@gmail.com>
2021-02-13 19:37:03 +0100

    add correct form request checker

--- app.py
+++ app.py
@@ -35,10 +35,16 @@ def items():
         return jsonify(response_data)
     elif request.method == 'POST':
         data = request.json
-        POSTS.append(data)
-        response_data['data'] = POSTS
-        response = jsonify(response_data)
-        response.status_code = 201
+        if 'id' not in data or 'name' not in data or 'last_name' not in data:
+            response_data['success'] = False
+            response_data['error'] = 'Please provide all required information'
+            response = jsonify(response_data)
+            response.status_code = 400
+        else:
+            POSTS.append(data)
+            response_data['data'] = POSTS
+            response = jsonify(response_data)
+            response.status_code = 201
         return response
 
 
